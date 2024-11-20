ceff3ea8aeb3de82e25772220cf3a48e3d63b896
Vaishnavi187 <vaishnavisingh2005.college@gmail.com>
2024-11-20 22:38:18 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
 
   return (
     <>
-      <div className="flex flex-wrap mr-14 justify-center gap-2 ml-32 mt-6">
+      <div className="flex flex-wrap mr-14 justify-center gap-2  mt-6">
         <button
           className="btn btn-outline px-4 py-2 rounded-md"
           onClick={() => setCategory("fashion")}
